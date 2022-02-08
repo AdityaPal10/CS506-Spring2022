975f1bd0611d764732887c31c993bdc43b4f3eae
Aditya Pal <pal.aditya.ap@gmail.com>
2022-02-08 11:54:17 -0500

    remove notimplemented error and add def

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,16 +1,27 @@
+import numpy as np
+from scipy import spatial
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
-        res += (x[i] - y[i])**2
-    return res**(1/2)
+        res += (x[i] - y[i]) ** 2
+    return res ** (1 / 2)
+
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
-def jaccard_dist(x, y):
-    raise NotImplementedError()
 
-def cosine_sim(x, y):
-    raise NotImplementedError()
+def jaccard_dist(list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+    return float(intersection) / union
+
+
+def cosine_sim(List1, List2):
+    result = 1 - spatial.distance.cosine(List1, List2)
+    return result
 
 # Feel free to add more
